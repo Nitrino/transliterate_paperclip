3979d05a54ac01153da1b890584e848da07fefb2
Nitrino <nitrinodev@gmail.com>
2013-08-27 16:01:30 +0400

    [fix] error toutf8

--- lib/transliterate_paperclip.rb
+++ lib/transliterate_paperclip.rb
@@ -2,7 +2,7 @@ require "transliterate_paperclip/version"
 
 module TransliteratePaperclip
   def translit_paperclip_file_name paperclip_image
-    image_file_name = self.attributes[paperclip_image.name.to_s + '_file_name'].toutf8
+    image_file_name = self.attributes[paperclip_image.name.to_s + '_file_name']
     ext = File.extname image_file_name
     basename = File.basename(image_file_name, ext)
     paperclip_image.instance_write(:file_name, Russian::translit(basename).to_url + ext)
