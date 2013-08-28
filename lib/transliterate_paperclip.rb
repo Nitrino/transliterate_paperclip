d39f6245511d6efe21f94f1e85af872f71c50086
Nitrino <nitrinodev@gmail.com>
2013-08-28 12:05:36 +0400

    [add] extending ActiveRecord::Base

--- lib/transliterate_paperclip.rb
+++ lib/transliterate_paperclip.rb
@@ -3,6 +3,7 @@ require 'russian'
 require 'stringex'
 
 module TransliteratePaperclip
+  extend ActiveSupport::Concern
   def translit_paperclip_file_name paperclip_image
     image_file_name = self.attributes[paperclip_image.name.to_s + '_file_name']
     ext = File.extname image_file_name
@@ -10,3 +11,4 @@ module TransliteratePaperclip
     paperclip_image.instance_write(:file_name, Russian::translit(basename).to_url + ext)
   end
 end
+ActiveRecord::Base.send(:include, TransliteratePaperclip)
